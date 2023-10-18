06920fa5fc62d9220c4ea2c6ad5537ce83bcc678
root <elrayah.alsanhory@gmail.com>
2023-10-17 18:58:34 -0700

    fourth try

--- newfile.c
+++ newfile.c
@@ -38,6 +38,10 @@ int _printf(const char *format, ...)
 
 	va_list args;
 
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
 	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
